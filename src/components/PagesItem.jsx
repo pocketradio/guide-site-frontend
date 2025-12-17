2d9a320ca0695cfd135d359d4b8fa0cbf0783f4d
Dylan Simon <grvdylans@gmail.com>
2025-12-17 10:33:24 -0700

    Improved form style controls on page edit

--- src/components/PagesItem.jsx
+++ src/components/PagesItem.jsx
@@ -25,22 +25,24 @@ export default function PagesItem({
         <li className="mt-4 w-full flex items-center" key={page.id}>
             {!editMode && <p>{page.title}</p>}
             {editMode && (
-                <input
-                    className="bg-(--red-brown) min-w-0 text-white px-2 box-border rounded flex-1 max-w-100 mr-2"
-                    type="text"
-                    value={inputText}
-                    onChange={(e) => setInputText(e.target.value)}
-                />
-            )}
-            {editMode && (
-                <button
-                    className="ml-auto mr-2 text-amber-50 bg-neutral-600 w-22 rounded px-2 py-0.5"
-                    onClick={() => {
-                        updatePageItem(page.id, inputText);
-                    }}
-                >
-                    Save
-                </button>
+                <form action="" className="w-full flex">
+                    <input
+                        className="bg-(--red-brown) min-w-0 text-white px-2 box-border rounded flex-1 max-w-100 mr-2"
+                        type="text"
+                        value={inputText}
+                        onChange={(e) => setInputText(e.target.value)}
+                    />
+                    <button
+                        className="mr-2 ml-auto text-amber-50 bg-neutral-600 w-22 rounded px-2 py-0.5"
+                        type="submit"
+                        onClick={(e) => {
+                            e.preventDefault();
+                            updatePageItem(page.id, inputText);
+                        }}
+                    >
+                        Save
+                    </button>
+                </form>
             )}
             <button
                 className={`${
