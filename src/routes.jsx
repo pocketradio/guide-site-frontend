4265ec691029fd528b3f2c6e318a240da8f419a1
Dylan Simon <grvdylans@gmail.com>
2026-01-05 16:45:02 -0700

    Add redirects from the original website to the new

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,8 +3,91 @@ import Main from "./components/Main";
 import PageManager from "./components/PageManager";
 import PageBuilder from "./components/PageBuilder";
 import EditorExample from "./components/TextEditor";
+import { Navigate } from "react-router";
+
+const oldRoutes = [
+    {
+        path: "/pages/stun-guide.html",
+        element: <Navigate to="/stun-guide" replace />,
+    },
+    {
+        path: "/pages/defense-reduction.html",
+        element: <Navigate to="/defense-reduction" replace />,
+    },
+    {
+        path: "/pages/sb-mg.html",
+        element: <Navigate to="/sb-mg" replace />,
+    },
+    {
+        path: "/pages/mythic-roles.html",
+        element: <Navigate to="/mythic-categories" replace />,
+    },
+    {
+        path: "/pages/mpregen.html",
+        element: <Navigate to="/mp-regen" replace />,
+    },
+    {
+        path: "/pages/atkspeed.html",
+        element: <Navigate to="/attack-speed" replace />,
+    },
+    {
+        path: "/pages/helpiamnew-quests.html",
+        element: <Navigate to="/newbie-quests" replace />,
+    },
+    {
+        path: "/pages/hell-mode-guide.html",
+        element: <Navigate to="/hell-mode-basics" replace />,
+    },
+    {
+        path: "/pages/hell-mode-gameplay-guide.html",
+        element: <Navigate to="/hell-mode" replace />,
+    },
+    {
+        path: "/pages/hell-mode-bosses.html",
+        element: <Navigate to="/hell-mode-bosses" replace />,
+    },
+    {
+        path: "/pages/magic-hell-build.html",
+        element: <Navigate to="/magic-hell-build" replace />,
+    },
+    {
+        path: "/pages/treasures.html",
+        element: <Navigate to="/exclusive-treasures" replace />,
+    },
+    {
+        path: "/pages/treasure-upgrade-costs.html",
+        element: <Navigate to="/treasure-upgrade-costs" replace />,
+    },
+    {
+        path: "/pages/how-to-upgrade-treasures.html",
+        element: <Navigate to="/unlock-treasures" replace />,
+    },
+
+    {
+        path: "/pages/safe-box-earnings.html",
+        element: <Navigate to="/safe-box-table" replace />,
+    },
+    {
+        path: "/pages/pet-list.html",
+        element: <Navigate to="/pets" replace />,
+    },
+    {
+        path: "/pages/daily-fortunes.html",
+        element: <Navigate to="/daily-fortunes" replace />,
+    },
+    {
+        path: "/pages/indy-treasures.html",
+        element: <Navigate to="/indy-treasures" replace />,
+    },
+
+    {
+        path: "/pages/unlock-order.html",
+        element: <Navigate to="/unlock-order" replace />,
+    },
+];
 
 const routes = [
+    ...oldRoutes,
     {
         path: "/",
         element: <Main />,
