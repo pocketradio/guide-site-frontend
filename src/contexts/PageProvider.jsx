061e4e0f857570d167ecd8298ef8a975a28d2524
cloudwolf <206946888+xerxes324@users.noreply.github.com.>
2026-02-05 07:20:26 +0530

    add navbar hydrate in useeffect

--- src/contexts/PageProvider.jsx
+++ src/contexts/PageProvider.jsx
@@ -1,35 +1,43 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
+import { hydrateNavbar } from "@/stores/navbarStore";
 
 const PageContext = createContext(null);
 
 export function PageProvider({ children }) {
-    const [title, setTitle] = useState("");
-    const [gameId, setGameId] = useState(1);
-    const serverAPI = "https://guide-site-backend.onrender.com";
-    const localAPI = "http://localhost:3000";
-    const currentAPI = import.meta.env.VITE_SERVER == "LOCAL" ? localAPI : serverAPI;
+	const [title, setTitle] = useState("");
+	const [gameId, setGameId] = useState(1);
 
-    return (
-        <PageContext.Provider
-            value={{
-                title,
-                setTitle,
-                currentAPI,
-                gameId,
-                setGameId,
-            }}
-        >
-            {children}
-        </PageContext.Provider>
-    );
+	const serverAPI = "https://guide-site-backend.onrender.com";
+	const localAPI = "http://localhost:3000";
+	const currentAPI = import.meta.env.VITE_SERVER == "LOCAL"
+		? localAPI
+		: serverAPI;
+
+	useEffect(() => {
+		hydrateNavbar(currentAPI);
+	}, [currentAPI]);
+
+	return (
+		<PageContext.Provider
+			value={{
+				title,
+				setTitle,
+				currentAPI,
+				gameId,
+				setGameId,
+			}}
+		>
+			{children}
+		</PageContext.Provider>
+	);
 }
 
 export function usePage() {
-    const context = useContext(PageContext);
+	const context = useContext(PageContext);
 
-    if (!context) {
-        throw new Error("usePage must be used within a PageProvider");
-    }
+	if (!context) {
+		throw new Error("usePage must be used within a PageProvider");
+	}
 
-    return context;
+	return context;
 }
