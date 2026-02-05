e66060be25dbdff8ec02f58ace3e073d4ffb7f7c
cloudwolf <206946888+xerxes324@users.noreply.github.com.>
2026-02-05 07:38:30 +0530

    dependencies and other minor code changes

--- src/routes.jsx
+++ src/routes.jsx
@@ -7,6 +7,7 @@ import oldRoutes from "./js/oldRoutes.jsx";
 import NotFound from "./components/NotFound.jsx";
 import GuardianCosts from "./components/mini-apps/GuardianCosts.jsx";
 import ImmortalGuardians from "./components/mini-apps/ImmortalGuardians.jsx";
+import NavigationPanel from "./components/NavigationPanel.jsx";
 const env = import.meta.env.VITE_ENV;
 
 const routes = [
@@ -32,7 +33,12 @@ if (env == "DEV") {
     routes[routes.length - 1].children.unshift({
         path: "page-manager/",
         element: <PageManager isAdmin={true} />,
+    },{
+        path : "navigation-panel/",
+        element : <NavigationPanel isAdmin = {true}/>
     });
+
+    routes[routes.length - 1]
 }
 
 export default routes;
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath } from "url";
+import path from "path";
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
-    test: {
-        globals: true,
-        environment: "jsdom",
-        setupFilse: "./src/tests/setup.js",
-    },
+	plugins: [react(), tailwindcss()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+	test: {
+		globals: true,
+		environment: "jsdom",
+		setupFiles: "./src/tests/setup.js",
+	},
 });
